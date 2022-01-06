3cc3018dd233db09be86a24efb9d6a1cb216bb95
Dani Santeugini <dani.santeugini@scopely.com>
2022-01-06 12:09:44 +0100

    Created Vehicle Showcase Shader

--- Assets/Scripts/Hovercabs/Controllers/VehicleShowcaseController.cs
+++ Assets/Scripts/Hovercabs/Controllers/VehicleShowcaseController.cs
@@ -1,15 +1,27 @@
 using System;
 using Hovercabs.Models;
+using Hovercabs.Views;
 using UnityEngine;
 
 namespace Hovercabs.Controllers
 {
+    [RequireComponent(typeof(VehicleShowcaseView))]
     public class VehicleShowcaseController : MonoBehaviour
     {
+        private VehicleShowcaseView _view;
         public float rotationSpeed;
-        
         [SerializeField] private string id;
         
+        private void Awake()
+        {
+            _view = GetComponent<VehicleShowcaseView>();
+        }
+
+        private void Start()
+        {
+            _view.Render();
+        }
+
         private void Update()
         {
             transform.Rotate(new Vector3(0,rotationSpeed*Time.deltaTime,0));
---
+++ Assets/Scripts/Hovercabs/Views/VehicleShowcaseView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace Hovercabs.Views
+{
+    public class VehicleShowcaseView : MonoBehaviour
+    {
+        private Material _showcaseMaterial;
+        private static readonly int IsAvailableProperty = Shader.PropertyToID("IsAvailable");
+
+        private void Awake()
+        {
+            _showcaseMaterial = GetComponent<Renderer>().sharedMaterial;
+        }
+
+        public void Render()
+        {
+            _showcaseMaterial.SetFloat(IsAvailableProperty,1f);         
+        }
+    }
+}
