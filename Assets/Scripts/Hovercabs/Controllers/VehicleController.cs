0bc8c00f643354409325eecad13f662de02be2aa
Dani Santeugini <dani.santeugini@scopely.com>
2022-01-06 21:13:36 +0100

    Added few UI elements in gameplay scene

--- Assets/Scripts/Hovercabs/Controllers/VehicleController.cs
+++ Assets/Scripts/Hovercabs/Controllers/VehicleController.cs
@@ -1,21 +1,29 @@
 ﻿using System;
+using Hovercabs.Views;
 using UnityEngine;
 
 namespace Hovercabs.Controllers
 {
     [RequireComponent(typeof(Rigidbody))]
     [RequireComponent(typeof(BoxCollider))]
+    [RequireComponent(typeof(VehicleView))]
     public class VehicleController : MonoBehaviour
     {
         [SerializeField] private float speed = 1.0f;
         private Rigidbody _rigidbody;
+        private VehicleView _view;
         
         private void Awake()
         {
             _rigidbody = GetComponent<Rigidbody>();
+            _view = GetComponent<VehicleView>();
         }
 
-        
+        private void Start()
+        {
+            _view.Render();
+        }
+
         private void Update()
         {
             if (Input.GetKey(KeyCode.UpArrow))
---
+++ Assets/Scripts/Hovercabs/Views/VehicleView.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Hovercabs.Views
+{
+    public class VehicleView: MonoBehaviour
+    {
+        private Material _vehicleMaterial;
+        private static readonly int IsAvailableProperty = Shader.PropertyToID("IsAvailable");
+        private static readonly int RotationSpeedProperty = Shader.PropertyToID("RotationSpeed");
+        //private static readonly int RotationAxisProperty = Shader.PropertyToID("RotationAxis");
+        
+        private void Awake()
+        {
+            _vehicleMaterial = GetComponent<Renderer>().sharedMaterial;
+        }
+        
+        public void Render()
+        {
+            _vehicleMaterial.SetFloat(IsAvailableProperty,1f);  
+            _vehicleMaterial.SetFloat(RotationSpeedProperty, 0f);
+        }
+    }
+}
